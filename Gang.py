d3536fbe22edb1612fe6da51fa5801d0cfa396d1
Faizulboften <72049391+Faizulboften@users.noreply.github.com>
2020-09-29 18:04:09 +0800

    Gang.py

---
+++ Gang.py
@@ -0,0 +1,176 @@
+█████████
+\033[1;93m█▄█████▄█      \033[1;91m●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●
+\033[1;93m█\033[1;92m▼▼▼▼▼ \033[1;92m- _ --_--\033[1;95m╔╦╗┌─┐┬─┐┬┌─   ╔═╗╔╗ 
+\033[1;93m█ \033[1;92m \033[1;92m_-_-- -_ --__\033[1;93m ║║├─┤├┬┘├┴┐───╠╣ ╠╩╗
+\033[1;93m█\033[1;92m▲▲▲▲▲\033[1;92m--  - _ --\033[1;96m═╩╝┴ ┴┴└─┴ ┴   ╚  ╚═╝ \033[1;96mtools-Dark
+\033[1;93m█████████      \033[1;92m«----------✧----------»
+\033[1;93m ██ ██
+\033[1;93m╔════════════════════════════════════════════╗
+\033[1;93m║\033[1;96m* \033[1;93mAuthor  \033[1;93m: \033[1;93mFaizul•Boften \033[1;93m               ║
+\033[1;93m║\033[1;96m* \033[1;93mGitHub  \033[1;93m: \033[1;93m\033[4mhttps://github.com/Faizulcyber  [ \033[1;93m  ║
+\033[1;93m╚════════════════════════════════════════════╝"
+echo ""
+echo "╔══════════════════════════════╗"
+toilet -f pagga "   MENU HACKING SOB!!!"| lolcat
+echo "╚══════════════════════════════╝"
+echo $cy "
+{✔ menu hacking sob ✔}
+[1] Tools Spam Bom Virus ✔
+[2] Hack Facebook  [-Target-] ✔
+[3] Sadap Watsapps ✔
+[4] Kumpulan Tools Linux ✔
+[5] Crack Hass Ban Pass FB ✔
+[6] Hack Akun Ml/FF ✔
+[7] Hack Facebook Target ✔
+[8] Dark v.1.2 ✔
+[9] v2.Dark ✔
+[10] Dark-Cyber11 ✔
+[11] cyber 2.5 ✔
+[12] IBLI2
+[13] Dark-Asu
+[14] BHV26
+[15] MR.Rendy-Dark
+[16] Clon Ind No Login
+[17] Clon Akun Old
+┗[99] exit ✘"
+echo '\033[35;1m'
+read -p "root@Pilih Nomor > " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+pkg install git
+git clone https://github.com/InYourG00D1/Spam
+cd Spam
+sh ms.sh
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+pkg install git
+git clone https://github.com/MRA27/FBNEWV2
+cd FBNEWV2
+python2 MRA27.py
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+pkg install git
+git clone https://github.com/Bl4ckDr460n/HxWhatsApp
+cd HxWhatsapp
+chmod +x HxWhatsApp
+python2 HxWhatsApp.py
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+pkg install git
+git clone https://github.com/Mrcakil/mrcakil
+cd mrcakil
+chmod +x tools
+./tools
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+pkg install git
+git clone https://github.com/FajriHidayat088/FHX-Hash-Killer/
+cd FHX-Hash-Killer
+python2 FHXHashKiller.py
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+pkg install git
+git clone https://github.com/Senitopeng/PhisingGame.git
+cd PhisingGame
+python2 phising.py
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+apt install python2
+pip2 install mechanize
+git clone https://github.com/FR13ND8/BRUTEFORCEnew,
+cd BRUTEFORCEnew
+sh new.sh
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+git clone https://github.com/cyber2611/v1.2
+ls
+cd v1.2
+ls
+python2 Dragon-v1.2.py
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+git clone https://github.com/cyber2611/v2-dark
+cd v2.dark
+ls
+python2 v2.dark.py
+if
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+git clone https://github.com/cyber2611/dark-cyber11
+cd dark-cyber11
+ls
+python2 kapten.py
+if
+
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+clear
+git clone https://github.com/cyber2611/cyber2.5
+cd cyber2.5
+ls
+python2 Premium2.5.py
+if
+
+if [ $bro = 12 ] || [ $bro = 12 ]
+then
+clear
+git clone https://github.com/cyber2611/IBLI2
+cd IBLI2
+ls
+python2 dark-iblis.py
+if
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then
+clear
+git clone https://github.com/cyber2611/dark-asu
+cd dark-asu
+ls
+python2 kimak.py
+if
+
+if if [ $bro = 14 ] || [ $bro = 14 ]
+then
+clear
+git clone https://github.com/cyber2611/BHV26
+cd BHV26
+ls
+python2 vip-1.9.py
+if
+
+if [ $bro = 15 ] || [ $bro = 15 ]
+then
+clear
+git clone https://github.com/cyber2611/Mr.RENDY
+cd Mr.RENDY
+ls
+python2 Mr.rendy.py
+if
